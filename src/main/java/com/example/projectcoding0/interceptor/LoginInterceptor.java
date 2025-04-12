546b73da4b36b264a7121532970d296d40610972
SeaSoda <1271164498@qq.com>
2025-04-12 23:53:52 +0800

    Backup on 2025-4-12

--- src/main/java/com/example/projectcoding0/interceptor/LoginInterceptor.java
+++ src/main/java/com/example/projectcoding0/interceptor/LoginInterceptor.java
@@ -1,13 +1,14 @@
-//package com.example.projectcoding0.interceptor;
-//
-//import jakarta.servlet.http.HttpServletRequest;
-//import jakarta.servlet.http.HttpServletResponse;
-//import org.springframework.web.servlet.HandlerInterceptor;
+package com.example.projectcoding0.interceptor;
 
-//public class LoginInterceptor implements HandlerInterceptor {
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+
+public class LoginInterceptor implements HandlerInterceptor {
 //    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
 //        System.out.println("LoginInterceptor");
 //        return true;
 //    }
-//
-//}
+
+}
